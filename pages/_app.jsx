cf8bb211af77bbbb46f2d66f74f7457e50a7b95a
Henrique Dornel <henrique.dornel.hds@gmail.com>
2021-03-11 13:33:43 -0300

    Final da Aula 3

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -8,47 +8,38 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
   body {
-    display: flex;
-    flex-direction: column;
     margin: 0;
     padding: 0;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
     font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
     color: ${({ theme }) => theme.colors.contrastText};
   }
   html, body {
     min-height: 100vh;
   }
   #__next {
+    flex: 1;
     display: flex;
     flex-direction: column;
-    flex: 1;
   }
 `;
 
 const { theme } = db;
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Quiz</title>
-        <meta name="title" content="Quiz" />
-        <meta name="description" content="" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://quiz-henriquedornel.vercel.app/" />
-        <meta property="og:title" content="Quiz" />
-        <meta property="og:description" content="" />
-        <meta property="og:image" content={db.bg} />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://quiz-henriquedornel.vercel.app/" />
-        <meta property="twitter:title" content="Quiz" />
-        <meta property="twitter:description" content="" />
-        <meta property="twitter:image" content={db.bg} />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </ThemeProvider>
     </>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 import db from '../db.json';
@@ -8,13 +9,15 @@ import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
-/* const BackgroundImage = styled.div`
-  background-image: url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
-`; */
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
 
 const QuizContainer = styled.div`
   width: 100%;
@@ -28,30 +31,37 @@ const QuizContainer = styled.div`
 `;
 
 export default function Home() {
-  const router = useRouter(); // hook
-  const [name, setName] = React.useState(''); // hook
+  const router = useRouter();
+  const [name, setName] = React.useState('');
 
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>{db.title}</title>
+      </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
           <Widget.Header>
-            <h1>Teste</h1>
+            <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <form onSubmit={(e) => {
-              e.preventDefault();
+            <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
               router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
             }}
             >
-              <input
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Diz aí o seu nome"
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Diz ai seu nome"
+                value={name}
               />
-              <button type="submit" disabled={name.trim().length === 0}>
+              <Button type="submit" disabled={name.length === 0}>
                 {`Jogar ${name}`}
-              </button>
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
@@ -60,12 +70,12 @@ export default function Home() {
           <Widget.Content>
             <h1>Quizes da Galera</h1>
 
-            <p>bla bla bla</p>
+            <p>lorem ipsum dolor sit amet...</p>
           </Widget.Content>
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/henriquedornel" />
+      <GitHubCorner projectUrl="https://github.com/omariosouto" />
     </QuizBackground>
   );
 }
