a3c98337ee509246e3f1360e5c6d63c92b219dc9
Henrique Dornel <henrique.dornel.hds@gmail.com>
2021-03-24 17:10:34 -0300

    Custom 404 and 500 error pages

--- pages/404.jsx
+++ pages/404.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import Error from '../src/components/Error';
 
 export default function Error404() {
-  return <Error />;
+  return <Error error={404} msg="Página não encontrada :(" />;
 }
--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import Head from '../Head';
 import Background from '../Background';
 import Container from '../Container';
 import Widget from '../Widget';
@@ -8,18 +8,17 @@ import Link from '../Link';
 
 import db from '../../../db/main.json';
 
-export default function Error() {
+export default function Error({ error, msg }) {
   return (
     <>
-      <Head {...db} />
       <Background backgroundImage={db.bg}>
         <Container>
           <Widget>
             <Widget.Header>
-              <h3>Ops!</h3>
+              <h3>{`Ops! - Erro ${error}`}</h3>
             </Widget.Header>
             <Widget.Content>
-              <p>Quiz não encontrado :(</p>
+              <p>{msg}</p>
               <Link href="/" style={{ color: '#fff', textDecoration: 'none' }}>
                 <Widget.Button type="button">
                   Procurar outros quizes
@@ -32,3 +31,8 @@ export default function Error() {
     </>
   );
 }
+
+Error.propTypes = {
+  error: PropTypes.number.isRequired,
+  msg: PropTypes.string.isRequired,
+};
